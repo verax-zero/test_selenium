38311eac5cbaecfc8d5516e103d3080601766741
Alex <pan666@list.ru>
2021-09-26 14:13:42 +0300

    Финальная версия

--- test_product_page.py
+++ test_product_page.py
@@ -6,7 +6,7 @@ import pytest
 import time
 
 
-@pytest.mark.login
+@pytest.mark.need_review
 class TestUserAddToBasketFromProductPage():
 
 	
@@ -46,6 +46,7 @@ class TestUserAddToBasketFromProductPage():
 		"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 		"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
 		page = ProductPage(browser, link)
 		page.open()
@@ -83,13 +84,15 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/ru/catalogue/hacking-exposed-wireless_208/"
 	page = BasketPage(browser, link)
 	page.open()
